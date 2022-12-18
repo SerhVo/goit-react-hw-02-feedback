7cd538b6e1536c73d2721d983a896051a07596fa
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-12-18 20:27:25 +0200

    hw-2.1

--- src/components/Feedback/Feedback.styled.js
+++ src/components/Feedback/Feedback.styled.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 export const BtnWrap = styled.div`
 display: flex;
 justify-content: space-around;
-gap: 20px;
+gap: 10px;
 `;
 
 export const BtnFeedback = styled.button`
@@ -10,11 +10,14 @@ display: flex;
 flex-direction: row;
 border: none;
 justify-content: center;
+align-items: center;
+border-radius: 4px;
 cursor: pointer;
-width: 60px;
+width: 80px;
+height: 30px;
 
 &:hover {
 scale: 1.1;
-background-color: #2174fa;
+background-color: #21bdfa;
 }
 `;
--- src/components/Feedback/feedback.jsx
+++ src/components/Feedback/feedback.jsx
@@ -1,10 +1,63 @@
+import Section from "components/Section/Section";
+import Notification from "components/Notification/Notification";
+import Statistics from "components/Statistics/Statistics";
+
 import {
     BtnFeedback, BtnWrap
 } from "./Feedback.styled";
 
 import PropTypes from 'prop-types';
 
-const Feedback = ({ option, onLeaveFeedback }) => {
+import React, { Component } from 'react';
+
+class Feedback extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    leaveFeedback = event => {
+        const value = event.target.innerText;
+        this.setState(prevState => ({ [value]: prevState[value] + 1 }));
+    };
+
+    countTotalFeedback = () =>
+        this.state.good + this.state.neutral + this.state.bad;
+    countPositiveFeedbackPercentage = () =>
+        Math.round((this.state.good * 100) / this.countTotalFeedback());
+
+    render() {
+        const { good, neutral, bad } = this.state;
+        const option = ['good', 'neutral', 'bad'];
+        return (
+            <>
+                <Section title="Please leave feedback">
+                    <FeedbackOption
+                        option={option}
+                        onLeaveFeedback={this.leaveFeedback}
+                    />
+                </Section>
+                <Section title="Statistics">
+                    {this.countTotalFeedback() ? (
+                        <Statistics
+                            good={good}
+                            neutral={neutral}
+                            bad={bad}
+                            total={this.countTotalFeedback()}
+                            positivePercentage={this.countPositiveFeedbackPercentage()}
+                        />
+                    ) : (
+                        <Notification message=":( There is no feedback" />
+                    )}
+                </Section>
+            </>
+        );
+    }
+}
+
+
+const FeedbackOption = ({ option, onLeaveFeedback }) => {
     return (
         <>
             <BtnWrap>
@@ -14,8 +67,9 @@ const Feedback = ({ option, onLeaveFeedback }) => {
     );
 };
 
-Feedback.propTypes = {
+FeedbackOption.propTypes = {
     option: PropTypes.array.isRequired,
     onLeaveFeedback: PropTypes.func.isRequired,
 };
+
 export default Feedback;
