2a425f89cf84d930c6ce67e4708100e91f5750c5
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-12-19 17:29:36 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,9 @@
-import Feedback from "./Feedback/feedback";
+import FeedbackOption from "./Feedback/feedback";
+import React, { Component } from 'react';
+import Section from "components/Section/Section";
+import Notification from "components/Notification/Notification";
+import Statistics from "components/Statistics/Statistics";
+
 
 export const App = () => {
   return (
@@ -7,3 +12,53 @@ export const App = () => {
     </div>
   );
 };
+
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = event => {
+    const value = event.target.innerText;
+    this.setState(prevState => ({ [value]: prevState[value] + 1 }));
+  };
+
+  countTotalFeedback = () =>
+    this.state.good + this.state.neutral + this.state.bad;
+  countPositiveFeedbackPercentage = () =>
+    Math.round((this.state.good * 100) / this.countTotalFeedback());
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const option = ['good', 'neutral', 'bad'];
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOption
+            option={option}
+            onLeaveFeedback={this.leaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message=":( There is no feedback" />
+          )}
+        </Section>
+      </>
+    );
+  }
+};
+
+
+
--- src/components/Feedback/feedback.jsx
+++ src/components/Feedback/feedback.jsx
@@ -1,60 +1,12 @@
-import Section from "components/Section/Section";
-import Notification from "components/Notification/Notification";
-import Statistics from "components/Statistics/Statistics";
 
 import {
     BtnFeedback, BtnWrap
 } from "./Feedback.styled";
-
+import App from 'components/App';
 import PropTypes from 'prop-types';
 
-import React, { Component } from 'react';
 
-class Feedback extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-    };
 
-    leaveFeedback = event => {
-        const value = event.target.innerText;
-        this.setState(prevState => ({ [value]: prevState[value] + 1 }));
-    };
-
-    countTotalFeedback = () =>
-        this.state.good + this.state.neutral + this.state.bad;
-    countPositiveFeedbackPercentage = () =>
-        Math.round((this.state.good * 100) / this.countTotalFeedback());
-
-    render() {
-        const { good, neutral, bad } = this.state;
-        const option = ['good', 'neutral', 'bad'];
-        return (
-            <>
-                <Section title="Please leave feedback">
-                    <FeedbackOption
-                        option={option}
-                        onLeaveFeedback={this.leaveFeedback}
-                    />
-                </Section>
-                <Section title="Statistics">
-                    {this.countTotalFeedback() ? (
-                        <Statistics
-                            good={good}
-                            neutral={neutral}
-                            bad={bad}
-                            total={this.countTotalFeedback()}
-                            positivePercentage={this.countPositiveFeedbackPercentage()}
-                        />
-                    ) : (
-                        <Notification message=":( There is no feedback" />
-                    )}
-                </Section>
-            </>
-        );
-    }
-}
 
 
 const FeedbackOption = ({ option, onLeaveFeedback }) => {
@@ -72,4 +24,4 @@ FeedbackOption.propTypes = {
     onLeaveFeedback: PropTypes.func.isRequired,
 };
 
-export default Feedback;
+export default FeedbackOption
